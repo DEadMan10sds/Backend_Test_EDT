4364cccf742f7e3f7c14bd8fefa05db440be95ba
DEadMan10sds <alejandroadan.sds@gmail.com>
2024-05-03 15:58:32 -0600

    ADD Frontend build on public folder REMOVED: Old documentation video comment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,26 +6,6 @@ import { content } from "./data/data.ts";
 import { JwtComponent } from "./pages/jwt/Jwt.tsx";
 import { Information } from "./interfaces/InformationDisplay.ts";
 
-/**
- import ReactPlayer from "react-player";
- {
-            path: "video",
-            element: (
-              <div className="grid gap-4">
-                <ReactPlayer
-                  className="max-w-64 md:max-w-lg lg:max-w-xl "
-                  url={[{ src: "/VideoPruebaTecnica.mp4", type: "video/mp4" }]}
-                  controls
-                  volume={0.5}
-                />
-                <a href="https://drive.google.com/file/d/12p3zgvh9KDuVPKwAMIioTiBUhs6AnX2D/view?usp=drive_link">
-                  Vídeo en Google drive
-                </a>
-              </div>
-            ),
-          },
- */
-
 const routes = content.map((value) => {
   const currentData: Information[] = value.data as Information[];
   if (value.text === "Home")
