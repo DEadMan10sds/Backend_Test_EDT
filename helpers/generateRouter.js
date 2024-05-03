e09e61956c2ad6a7baefdf79b7eb18810db7071f
DEadMan10sds <alejandroadan.sds@gmail.com>
2024-05-02 20:11:20 -0600

    ADD: Auto generation of routers function

---
+++ helpers/generateRouter.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+export default function generateRouter(routesArray) {
+  const genericRouter = Router();
+
+  routesArray.forEach((route) => {
+    genericRouter[route.type](route.route, route.middlewares, route.function);
+  });
+
+  return genericRouter;
+}
